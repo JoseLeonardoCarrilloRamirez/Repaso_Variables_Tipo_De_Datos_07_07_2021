6808c07b11801beb7906becae574ba49669f50da
JoseLeonardoCarrilloRamirez <joseleonardocarrillo@gmail.com>
2021-08-30 19:29:57 -0500

    Repaso Variables 20210707

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+nombreCompletoDelAlumno = `Jose Leonardo Carrillo Ramirez :V`;
+miEdad = 30;
+// altura = 1.43;
+soyUnProfesor = false;
+
+
+console.log(nombreCompletoDelAlumno, typeof(nombreCompletoDelAlumno));
+console.log(miEdad, typeof(miEdad));
+// console.log(altura, typeof(altura));
+console.log(soyUnProfesor, typeof(soyUnProfesor));
+
+
+// alert()
+// document.write();
